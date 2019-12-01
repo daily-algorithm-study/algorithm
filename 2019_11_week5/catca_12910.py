09e8f39071c493e008b7837f47596a94e00217cf
catca <57560457+catca@users.noreply.github.com>
2019-12-01 20:21:56 +0900

    Create catca_12910.py

---
+++ 2019_11_week5/catca_12910.py
@@ -0,0 +1,11 @@
+#나누어 떨어지는 숫자 배열
+
+def solution(arr, divisor):
+    answer = []
+    for i in range(len(arr)):
+        if arr[i] % divisor == 0:
+            answer.append(arr[i])
+    answer.sort()
+    if len(answer) == 0:
+        answer.append(-1)
+    return answer
